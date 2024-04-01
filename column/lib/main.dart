2c953f130f6822a2250f6d14d6730ef29296de11
Hikmabalqis <balqishikmatullah7@gmail.com>
2024-04-01 23:50:51 +0700

    nambah column

---
+++ column/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Layout Row dan Column"),),
+        body: Column(
+          children: [
+          Text("text 1"),
+          Text("text 2"),
+          Text("text 3"),
+        ],),
+      ),
+    );
+  }
+}
